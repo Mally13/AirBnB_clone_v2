b91bcfbf059e989a6f48236f8e497d664a81b01d
Mally13 <mallymutuku13@gmail.com>
2024-01-28 16:11:00 +0300

    Task 0: Setup Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello_world():
-	"""Displays the hello world message"""
-	return "<p>Hello HBNB!</p>"
+	"""Displays the message Hello HBNB"""
+	return "Hello HBNB!"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)

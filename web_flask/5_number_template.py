15f13d785a8e842b28268f55f2cfea14524c69e5
Mally13 <mallymutuku13@gmail.com>
2024-01-28 18:34:20 +0300

    Task 4: Route parameters and templates

---
+++ web_flask/5_number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Hello Flask"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Displays the message Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_world_2():
+    """Displays the message HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hello_world_3(text):
+    """Displays the message C with parameter text"""
+    normal_text = text.replace('_', " ")
+    return f'C {normal_text}'
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def hello_world_4(text="is cool"):
+    """Displays the message Python with parameter text"""
+    text = text.replace('_', " ")
+    return f'Python {text}'
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def hello_world_5(n):
+    """Displays the message if a parameter is a number"""
+    return f'{n} is a number'
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def hello_world_6(n):
+    """Displays an html page"""
+    return render_template("./templates/5-number.html", number=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

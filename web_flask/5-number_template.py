402bd6fb1c3b3ae20ff6d13ad9eddcf05d24caa1
Mally13 <mallymutuku13@gmail.com>
2024-01-28 18:51:17 +0300

    Task 5: Route parameters and templates

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,7 +42,7 @@ def hello_world_5(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def hello_world_6(n):
     """Displays an html page"""
-    return render_template("5-number.html", number=n)
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == '__main__':

200f3d6e88d4c8e47e70a8c39203ef6efe028bc3
Mally13 <mallymutuku13@gmail.com>
2024-01-29 16:30:42 +0300

    Task 7 correction 3

--- fabfile.py
+++ fabfile.py
@@ -8,13 +8,13 @@ A fabric script that compresses contents of webstatic folder
 
 
 def do_pack():
-	"""
-	Generates a .tgz archive
-	"""
-	try:
-		local("mkdir -p versions")
-		zip_name=f"versions/web_static_{current_datetime.strftime('%Y%m%d%H%M%S')}.tgz"
-		local(f"tar -czvf {zip_name} webstatic")
-		return zip_name
-	except Exception as e:
-		return None
+    """
+    Generates a .tgz archive
+    """
+    try:
+        local("mkdir -p versions")
+        zip_name = f"versions/web_static_{current_datetime.strftime('%Y%m%d%H%M%S')}.tgz"
+        local(f"tar -czvf {zip_name} webstatic")
+        return zip_name
+    except Exception as e:
+        return None
